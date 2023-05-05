72944dae14fac41a5f8167e1eafd024d88f86f42
YaozhenghangMa <Yaozhenghang.Ma@gmail.com>
2023-05-05 15:53:52 +0800

    init and apply MIT License

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# MatField
+# hybrid_programming
 
-[![Build Status](https://github.com/YaozhenghangMa/matfield/actions/workflows/CI.yml/badge.svg?branch=)](https://github.com/YaozhenghangMa/matfield/actions/workflows/CI.yml?query=branch%3A)
-[![Coverage](https://codecov.io/gh/YaozhenghangMa/matfield/branch/main/graph/badge.svg)](https://codecov.io/gh/YaozhenghangMa/matfield)
-[![PyPI](https://img.shields.io/pypi/v/matfield)](https://pypi.org/project/matfield/)
-![License](https://img.shields.io/github/license/yaozhenghangma/matfield)
+[![Build Status](https://github.com/YaozhenghangMa/hybrid_programming/actions/workflows/CI.yml/badge.svg?branch=)](https://github.com/YaozhenghangMa/hybrid_programming/actions/workflows/CI.yml?query=branch%3A)
+[![Coverage](https://codecov.io/gh/YaozhenghangMa/hybrid_programming/branch/main/graph/badge.svg)](https://codecov.io/gh/YaozhenghangMa/hybrid_programming)
+[![PyPI](https://img.shields.io/pypi/v/hybrid_programming)](https://pypi.org/project/hybrid_programming/)
+![License](https://img.shields.io/github/license/yaozhenghangma/hybrid_programming)
--- setup.py
+++ setup.py
@@ -13,16 +13,16 @@ except ImportError:
 from setuptools import find_packages
 
 setup(
-    name="matfield",
+    name="hybrid_example",
     version="0.0.1",
     description="Dynamical mean field theory package for material research.",
     author="Yaozhenghang Ma",
     author_email="Yaozhenghang.Ma@gmail.com",
     license="GNU GPLv3",
-    url="https://github.com/yaozhenghangma/matfield/",
+    url="https://github.com/yaozhenghangma/hybrid_programming/",
     project_urls={
-        "Bug Tracker": "https://github.com/yaozhenghangma/matfield/issues/",
-        "Documentation": "https://matfield.readthedocs.io/",
+        "Bug Tracker": "https://github.com/yaozhenghangma/hybrid_programming/issues/",
+        "Documentation": "https://hybrid_programming.readthedocs.io/",
     },
     install_requires=[
         "setuptools>=42",
@@ -33,7 +33,7 @@ setup(
     ],
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    cmake_install_dir="src/matfield",
+    cmake_install_dir="src/hybrid_example",
     include_package_data=True,
     extras_require={"test": ["pytest"]},
     python_requires=">=3.8",
